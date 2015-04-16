43db7afea61dbfb9382a80ad36243541635b6fb2
pedrocastellucci <pedrocastellucci@github.com>
2015-04-15 23:48:36 -0300

    Copying instead of moving .emacs and .emacs.d and adding other files~

---
+++ dropboxInstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
+sudo apt-get update
+sudo apt-get install nautilus-dropbox
---
+++ repositoryInstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Text editors:
+sudo apt-get install emacs 
+sudo apt-get install vim
+
+# Python installs:
+sudo apt-get install python-pip
+sudo apt-get install python-jedi
+sudo apt-get install python-matplotlib
+sudo apt-get install python-pandas
+sudo apt-get install python-sklearn
+
+# For converting ipython notebooks to other formats:
+sudo apt-get install pandoc
+
+# IPython notebook install:
+sudo pip install ipython[all]
+
+# Others:
+sudo apt-get install build-essentials
+sudo apt-get install gimp
+sudo apt-get install inkscape
+sudo apt-get install pidgin
+sudo apt-get install vlc
+sudo apt-get install texlive-full
