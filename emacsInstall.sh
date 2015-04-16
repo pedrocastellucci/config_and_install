1dffe7090c6ed808aaf10ad2386f9218ed1fb2c1
pedrocastellucci <pedrocastellucci@github.com>
2015-04-15 23:55:29 -0300

    Copying instead of moving .emacs and .emacs.d and fixed a bug in gurobiInstall

--- emacsInstall.sh
+++ emacsInstall.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install emacs 
 git clone https://github.com/pedrocastellucci/emacs.config
-mv emacs.config/.emacs ~/.
-mv emacs.config/.emacs.d ~/.
+cp emacs.config/.emacs ~/.
+cp emacs.config/.emacs.d ~/.
